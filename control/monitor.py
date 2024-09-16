b7f37867eb78d138edb8be0bad14e643d0701dd9
YeiHong <yh.zhangc@uniandes.edu.co>
2024-09-16 16:53:31 -0500

    update alert app

--- control/monitor.py
+++ control/monitor.py
@@ -10,44 +10,6 @@ from django.conf import settings
 
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1, settings.MQTT_USER_PUB)
 
-def alerta_peligro():
-    print("Calulando alertas de peligro...")
-    data = Data.objects.filter(
-        base_time__gte=datetime.now() - timedelta(hours=1))
-    aggregation = data.annotate(check_value=Avg('avg_value')) \
-        .select_related('station', 'measurement') \
-        .select_related('station__user', 'station__location') \
-        .select_related('station__location__city', 'station__location__state',
-                        'station__location__country') \
-        .values('check_value', 'station__user__username',
-                'measurement__name',
-                'measurement__max_value',
-                'measurement__min_value',
-                'station__location__city__name',
-                'station__location__state__name',
-                'station__location__country__name')
-    for item in aggregation:
-        alerta = False
-
-        medidad = item["measurement_name"]
-        max_valor = item["measurement__max_value"] or 0
-
-        country = item['station__location__country__name']
-        state = item['station__location__state__name']
-        city = item['station__location__city__name']
-        user = item['station__user__username']
-
-        if medidad == "temperatura" and max_valor > 39:
-            alerta = True
-        
-        if alerta:
-            message = "ALERTA! la temperatura ha alcanzado {max_valor}"
-            topic = '{}/{}/{}/{}/in'.format(country, state, city, user)
-            print(datetime.now(), "Sending alert to {} {}".format(topic, medidad))
-            client.publish(topic, message)
-
-
-
 def analyze_data():
     # Consulta todos los datos de la última hora, los agrupa por estación y variable
     # Compara el promedio con los valores límite que están en la base de datos para esa variable.
@@ -72,6 +34,7 @@ def analyze_data():
     alerts = 0
     for item in aggregation:
         alert = False
+        alerta = False
 
         variable = item["measurement__name"]
         max_value = item["measurement__max_value"] or 0
@@ -85,13 +48,24 @@ def analyze_data():
         if item["check_value"] > max_value or item["check_value"] < min_value:
             alert = True
 
+        if item["measurement__name"] == "temperatura" and max_value > 39.0:
+            alerta = True
+
+        if alerta:
+            message = "ALERT {} {}".format(variable, max_value)
+            topic = '{}/{}/{}/{}/in'.format(country, state, city, user)
+            print(datetime.now(), "Sending alert to {} {}".format(topic, variable))
+            client.publish(topic, message)
+            alerts += 1
+
         if alert:
             message = "ALERT {} {} {}".format(variable, min_value, max_value)
             topic = '{}/{}/{}/{}/in'.format(country, state, city, user)
             print(datetime.now(), "Sending alert to {} {}".format(topic, variable))
             client.publish(topic, message)
             alerts += 1
-
+    
+    print("Alerta 2 is {alerta}")
     print(len(aggregation), "dispositivos revisados")
     print(alerts, "alertas enviadas")
 
@@ -142,9 +116,7 @@ def start_cron():
     Inicia el cron que se encarga de ejecutar la función analyze_data cada 5 minutos.
     '''
     print("Iniciando cron...")
-    schedule.every(5).minutes.do(analyze_data)
-    print("Iniciando segunda alarma...")
-    schedule.every(1).minutes.do(alerta_peligro)
+    schedule.every(1).minutes.do(analyze_data)
     print("Servicio de control iniciado")
 
     while 1:
